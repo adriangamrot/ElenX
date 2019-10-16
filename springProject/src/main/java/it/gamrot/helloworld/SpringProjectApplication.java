346b63d32fcd1f0ea266a4e8e2abc57a416ecbb4
Adrian Gamrot <adriangamrot@gmail.com>
2019-10-16 14:14:03 +0200

    Spring Hello World

---
+++ springProject/src/main/java/it/gamrot/helloworld/SpringProjectApplication.java
@@ -0,0 +1,15 @@
+package it.gamrot.helloworld;
+
+//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SpringProjectApplication.class, args);
+		System.out.println("Hello World!");
+		
+	}
+
+}
